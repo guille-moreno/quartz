864a006b971a04054efbf63f31e36f8493bd0174
Guillermo <guillemoreno97@gmail.com>
2025-08-22 14:10:32 +0200

    Quartz sync: Aug 22, 2025, 2:10 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,8 @@ export default ((opts?: Options) => {
     return (
       
       <footer class={`${displayClass ?? ""}`}>
-        <div id="remark42"></div>
         <p>
+          <div id="remark42"></div>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
